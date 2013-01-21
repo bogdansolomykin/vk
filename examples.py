99d5f7c81af140e0280b65af0af4704c4a6404f2
Bogdan Solomykin <bogdan.solomykin@gmail.com>
2013-01-22 00:55:05 +0200

    modification descriptions

--- examples.py
+++ examples.py
@@ -1,17 +1,17 @@
 import vk
 
 params = {
-    "client_id": "111111",
-    "scope": ("friends", "audio")    
-}
+    "client_id": "111111",         #your application id
+    "scope": ("friends", "audio")  #access rights requested by your app
+}                                  #see https://vk.com/developers.php?oid=-17680044&p=Application_Access_Rights
 
 #auth
 auth = vk.VK_AUTH(params)
-auth.authorization('your_mail@gmail.com', 'password')
+auth.authorization('user_mail@gmail.com', 'password')
 access_token = auth.get_access_token()
 user_id = auth.get_user_id()
 
-#api request
+#api requests
 api = VK_API(access_token, user_id)
 user_info = api.request('users.get')
 user_audios = api.request('audio.get', uid=user_id})
