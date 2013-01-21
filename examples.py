106eb1b7426b763d8eac4480b8b128d47d85f18d
Bogdan Solomykin <bogdan.solomykin@gmail.com>
2013-01-22 00:03:11 +0200

    adding example for vk api wrapper

--- examples.py
+++ examples.py
@@ -5,8 +5,12 @@ params = {
     "scope": ("friends", "audio")    
 }
 
+#auth
 auth = vk.VK_AUTH(params)
 auth.authorization('your_mail@gmail.com', 'password')
 access_token = auth.get_access_token()
 user_id = auth.get_user_id()
-print access_token, user_id
+
+#api request
+api = VK_API(access_token, user_id)
+api.request('users.get', fields=("last_name", "first_name"))
